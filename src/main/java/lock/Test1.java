9f5a8f709167edb0878b67415928012548f5ee4d
talent.he <talent.liang.he@accenture.com>
2021-06-17 18:51:14 +0800

    trylock()

--- src/main/java/lock/Test1.java
+++ src/main/java/lock/Test1.java
@@ -4,6 +4,28 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/*
+
+	* Lock 与 synchronized 关键字在锁的处理上的重要差别
+
+		* 锁的获取方式
+
+			* Lock 是通过由程序代码的方式由开发者手工获取
+			* synchronized 是由JVM 自动处理，无需手工干预
+		* 具体实现方式
+
+			* Lock 是由Java 代码方式实现
+			* synchronized 是通过JVM底层实现
+		* 锁的释放
+
+			* lock 必须通过unlock方法在finally块中手工释放
+			* synchronized 由JVM 自动释放
+		* 锁的具体类型
+
+			* lock 提供了多种类型的锁，例如公平锁、非公平锁，也提供可重入锁.
+			* synchronized 提供了可重入锁
+
+ */
 public class Test1 {
 
     private final Lock lock = new ReentrantLock(); // 可重入锁
